b5ec09696ba79cee0ab809c0c50d553306b5ff79
Andhika Yoga Perdana <andhika.perdana@btpn.com>
2022-09-21 19:58:09 +0700

    fix problem

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (expression) => {
+  const ixxData = await promiseTheaterIXX()
+  const vgcData = await promiseTheaterIXX()
+
+  const ixxDataCount = ixxData.filter((element) => {
+    return element.hasil === expression;
+  }).length
+  const vgcDataCount = vgcData.filter((element) => {
+    return element.hasil === expression;
+  }).length
+
+  return ixxDataCount + vgcDataCount;
+};
 
 module.exports = {
   promiseOutput,
